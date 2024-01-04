943d7c5c1919c8c4ade7452b155ec2bfc912ef57
Count Dracul Yeamin the Scorpio <34183654+mushfiqur47@users.noreply.github.com>
2024-01-04 21:17:36 +0600

    Filter Position In a list

---
+++ Filter Position In a list.scala
@@ -0,0 +1,11 @@
+
+def f(arr: List[Int]): List[Int] = {
+  val l = scala.collection.mutable.ListBuffer.empty[Int]
+  var idx = 0
+  arr.foreach(i => {
+    idx += 1
+    if (idx % 2 == 0) l += i
+  })
+  
+  l.toList
+}

815ba1cfb870e89ecec27974f8d6853cbd0dc374
Count Dracul Yeamin the Scorpio <34183654+mushfiqur47@users.noreply.github.com>
2024-01-20 12:38:00 +0600

    Update Reverse a list.erl

--- Reverse a list.erl
+++ Reverse a list.erl
@@ -1,1 +1,24 @@
 
+-module(solution).
+-export([main/0]).
+
+read(T)->
+    case io:fread("","~d") of
+        {ok,[H]} -> read([H|T]);
+        _ ->
+            T
+        end.
+
+output(L,S) ->
+    if S=:=0->
+            ok;
+        S>-1 ->
+        [H|T] = L,
+        io:format("~p~n",[H]),
+        output(T,S-1)
+    end.
+
+main() ->
+    Li = read([]),
+    S=length(Li),
+    output(Li,S).

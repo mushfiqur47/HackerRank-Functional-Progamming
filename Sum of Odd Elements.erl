9fcba2735c34b254d6a8c08e322495ceb2406440
Count Dracul Yeamin the Scorpio <34183654+mushfiqur47@users.noreply.github.com>
2024-01-21 17:12:48 +0600

    Update Sum of Odd Elements.erl

--- Sum of Odd Elements.erl
+++ Sum of Odd Elements.erl
@@ -1,1 +1,14 @@
+-module(solution).
+-export([main/0]).
+
+read(Su) ->
+    case io:fread("", "~d") of
+        eof -> io:format("~p~n",[Su]);
+        {ok, [N]} -> if (N rem 2=/=0)-> read(Su+N);
+                         (N rem 2 =:=0)-> read(Su)
+            end
+    end.
+
+main() ->
+    read(0).
 

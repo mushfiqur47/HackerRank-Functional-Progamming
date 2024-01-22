e747529dfe0a29a3c568a540aacb5eb4d7418b40
Count Dracul Yeamin the Scorpio <34183654+mushfiqur47@users.noreply.github.com>
2024-01-23 00:16:08 +0600

    Update Update List.erl

--- Update List.erl
+++ Update List.erl
@@ -1,1 +1,22 @@
 
+-module(solution).
+-export([main/0]).
+
+read(NewList) -> 
+    case io:fread("", "~d") of 
+        {ok, [NewElement]} -> 
+            read(NewList ++ [NewElement]);
+        _ -> 
+            NewList end. 
+
+absValue(X) when X >= 0 ->
+    X;
+absValue(X) when X < 0 ->
+    X * -1;
+absValue(_) ->
+    ok.
+
+main() ->
+    List = read([]),
+    AbsList = [absValue(N) || N <- List],
+    [io:format("~p~n", [A]) || A <- AbsList].

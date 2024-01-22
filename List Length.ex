cfaecc779a8a3e17b217324e24b53ad6446f1d63
Count Dracul Yeamin the Scorpio <34183654+mushfiqur47@users.noreply.github.com>
2024-01-22 17:09:17 +0600

    Update List Length.ex

--- List Length.ex
+++ List Length.ex
@@ -1,1 +1,23 @@
 List 
+
+defmodule Arr do
+  def count(arr) do
+    c(arr, 0)
+  end
+
+  def c([_ | tail], acc) do
+    c(tail, acc + 1)
+  end
+
+  def c([], acc) do
+    acc
+  end
+end
+
+defmodule Solution do
+  IO.read(:all)
+  |> String.split
+  |> Enum.map(&String.to_integer/1)
+  |> Arr.count
+  |> IO.puts
+end

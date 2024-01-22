a2728fe5e09089a2820c629ab45ec854025fcb1a
Count Dracul Yeamin the Scorpio <34183654+mushfiqur47@users.noreply.github.com>
2024-01-22 17:05:24 +0600

    Update List Length.erl

--- List Length.erl
+++ List Length.erl
@@ -1,1 +1,14 @@
+%List Length
 
+-module(solution).
+-export([main/0]).
+
+counter(Curr)->
+    case io:fread("","~d") of
+        eof -> io:format("~p~n",[Curr]);
+        {ok,[N]}-> counter(Curr+1)
+    end.
+
+
+main() ->
+    counter(0).

1bcb85bfd4557a635ce672134f23d083acd933c5
Count Dracul Yeamin the Scorpio <34183654+mushfiqur47@users.noreply.github.com>
2024-01-25 22:18:14 +0600

    Update Number of Binary Search Tree.hs

--- Number of Binary Search Tree.hs
+++ Number of Binary Search Tree.hs
@@ -1,1 +1,11 @@
-
+bst :: Int -> Integer
+bst = (map f [0..] !!)
+    where
+        f 0 = 1
+        f 1 = 1
+        f n = sum [ bst (i-1) * bst (n-i) | i <- [1..n]] `mod` (10^8 + 7) 
+        
+main :: IO()
+main = do
+    raw <- getContents
+    mapM_ print $ map bst $ tail $ map (read :: String -> Int) $ lines raw

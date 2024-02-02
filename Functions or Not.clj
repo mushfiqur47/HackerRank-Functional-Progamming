4a1d1e0705c9636dc814994aac30c984ad6df752
Count Dracul Yeamin the Scorpio <34183654+mushfiqur47@users.noreply.github.com>
2024-02-02 17:45:44 +0600

    Update Functions or Not.clj

--- Functions or Not.clj
+++ Functions or Not.clj
@@ -1,1 +1,19 @@
 
+(ns lab
+  (:require [clojure.string :as str]))
+
+(defn read-value []
+  (-> (read-line) (str/split #" ") first Integer/parseInt))
+
+(defn func? [lst]
+  (if (apply distinct? lst) "YES" "NO"))
+
+(defn solution []
+  (let [N (read-value)]
+    (repeatedly N (fn []
+                    (let [n (read-value)
+                          v (repeatedly n #(read-value))]
+                      (func? v))))))```
+
+(doseq [ans (solution)]
+  (println ans))

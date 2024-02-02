ead0d4d698e18607de42fd6f47fc06e20db194e8
Count Dracul Yeamin the Scorpio <34183654+mushfiqur47@users.noreply.github.com>
2024-02-02 17:44:36 +0600

    Update Functions or Not.fs

--- Functions or Not.fs
+++ Functions or Not.fs
@@ -1,1 +1,27 @@
 
+open System
+
+let readDomain n =
+    [ 1..n ]
+    |> List.map (fun _ -> (Console.ReadLine().Split ' ').[0])
+
+let validate domain =
+    domain
+    |> List.distinct
+    |> List.length
+    |> (=) (List.length domain)
+
+[<EntryPoint>]
+let main argv =
+    let t = Console.ReadLine() |> int
+
+    [ 1..t ]
+    |> List.iter (fun _ ->
+        Console.ReadLine()
+        |> int
+        |> readDomain
+        |> validate
+        |> (fun isValid -> if isValid then "YES" else "NO")
+        |> Console.WriteLine)
+
+    0

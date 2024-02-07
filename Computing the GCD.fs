09aade2ae6eb03d950b00d08b7628973091b7ddc
Count Dracul Yeamin the Scorpio <34183654+mushfiqur47@users.noreply.github.com>
2024-02-07 13:39:20 +0600

    Computing the GCD

---
+++ Computing the GCD.fs
@@ -0,0 +1,30 @@
+open System
+
+let parseArrayToTuple (input: int array) =
+    (input.[0], input.[1])
+
+let getInput () =
+    Console.ReadLine().Split(" ")
+    |> Array.map int
+    |> parseArrayToTuple
+    
+let rec gcd (input: int * int) =
+    let mutable biggerNumber = 0
+    let mutable smallerNumber = 0
+    
+    if (fst input) > (snd input) then
+        biggerNumber <- (fst input)
+        smallerNumber <- (snd input)
+    else
+        biggerNumber <- (snd input)
+        smallerNumber <- (fst input)
+        
+    let divisionResult = biggerNumber % smallerNumber
+    
+    match (divisionResult = 0) with
+    | true -> smallerNumber
+    | false -> gcd (smallerNumber, divisionResult)
+    
+getInput ()
+|> gcd
+|> Console.WriteLine

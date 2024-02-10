34889fb3607f2b4755bba20f9b68f81e5a35a273
Count Dracul Yeamin the Scorpio <34183654+mushfiqur47@users.noreply.github.com>
2024-02-10 22:02:06 +0600

    Update String Mingling.ex

--- String Mingling.ex
+++ String Mingling.ex
@@ -1,1 +1,15 @@
 
+defmodule Solution do
+    def mingling([],[],str), do: str
+    def mingling([p1|p],[q1|q], str) do
+        mingling(p, q, str<>p1<>q1)
+    end
+    def solve(str) do
+        [p,q]= String.split(str,"\n")
+        mingling(String.graphemes(p), String.graphemes(q), "")
+    end
+end
+
+IO.read(:all)
+|> Solution.solve
+|> IO.puts

cc83cfaae1f08ac195e3b0fbe8c440a10853fa0f
Count Dracul Yeamin the Scorpio <34183654+mushfiqur47@users.noreply.github.com>
2024-02-24 20:24:37 +0600

    Super Digit

--- Super Digit.fs
+++ Super Digit.fs
@@ -1,1 +1,18 @@
 
+open System
+
+let rec reduce k (n: string) =
+    match n with
+    | n when String.length n = 1 -> n
+    | n ->
+        n
+        |> Seq.map string
+        |> Seq.map (fun c -> (int64 c) * k)
+        |> Seq.sum
+        |> string
+        |> reduce 1L
+
+
+let [| n; k |] = Console.ReadLine().Split(' ')
+
+reduce (int64 k) n |> Console.WriteLine
